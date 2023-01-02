4c2e18e927a16ecb01ee4adc2185b7cfd66e2571
PangZL <2078106843@qq.com>
2023-01-02 16:11:35 +0800

    项目框架搭建

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,41 @@
-<template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+<!-- 用来写html -->
+<template> 
+  <div>
+    <div>Bot昵称：{{bot_name}}</div>
+    <div>Bot战力：{{ bot_rating }}</div>
   </div>
-  <router-view/>
 </template>
 
+<!-- 用来写js -->
+<script>
+import $ from 'jquery';
+import { ref } from 'vue'; // 定义变量
+
+export default {
+    name: "App",
+    setup: () => { // 主函数入口
+      let bot_name = ref(""); // 变量
+      let bot_rating = ref("");
+
+      // 从后端返回数据
+      $.ajax({
+        type: "get",
+        url: "http://localhost:8080/pk/getbotinfo/",
+        data: "data",
+        dataType: "json",
+        success: function (response) { // 返回的数据存在response中
+          bot_name.value = response.name;
+          bot_rating.value = response.rating;
+        }
+      });
+      return { // 返回变量的值
+      bot_name,
+      bot_rating
+    }
+  }
+}
+</script>
+
+<!-- 用来写css -->
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
